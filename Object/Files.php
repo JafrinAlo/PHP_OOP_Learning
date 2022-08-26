5c62e84c49c5c8273f7e229fc75635740fdf7f6c
JafrinAlo <jafrinalo2020@gmail.com>
2022-08-26 08:24:28 +0600

    exercise 2 files.php added

--- Object/Files.php
+++ Object/Files.php
@@ -1,22 +1,40 @@
 <?php
-class Files{
-    function displayContent($filename):bool{
-    $result=true;
-    try{
-        echo "Read the content of the file: $filename".PHP_EOL;
-        $content=file_get_contents($filename);
-        echo "Displaying Content of the $filename".PHP_EOL;
-        echo $content;
-    }catch(Exception $e){
-        $result=false;
+class Files
+{
+    public function displayContent($filename)
+    {
+        if (!file_exists($filename)) {
+            echo "$filename doesnot exist".PHP_EOL;
+            return;
+        }
+        echo file_get_contents($filename).PHP_EOL;
+        /*$result=true;
+        try{
+            echo "Read the content of the file: $filename".PHP_EOL;
+            $content=file_get_contents($filename);
+            echo "Displaying Content of the $filename".PHP_EOL;
+            echo $content;
+        }catch(Exception $e){
+            $result=false;
+        }
+        return $result;*/
     }
-    return $result;
+
+    public function getContent($filename):string
+    {
+        if (!file_exists($filename))
+        {
+            echo "$filename doesnot exist".PHP_EOL;
+            return "";
+        }
+        $content=file_get_contents($filename).PHP_EOL;
+        return $content;
     }
 }
 
 $files1= new Files();
 $files1->displayContent("test.txt");
-
+echo "Here is test.txt ". $files1->getContent("test.txt").PHP_EOL;
 $files2=new Files();
 $files2->displayContent("test2.txt");
 
